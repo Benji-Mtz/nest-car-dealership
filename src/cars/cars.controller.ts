75d7e6f63df828dfd693af8020f6ead1fd83c5d8
Benji Martinez <benjim.flores@outlook.com>
2022-11-06 00:06:42 -0600

    add service for crud

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,25 +1,48 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
 
-  private cars = [ 'Chevrolet', 'Tesla', 'Mclaren' ];
+  constructor(private readonly carsService: CarsService) {
+
+  }
 
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAll();
   }
 
   @Get(':id')
-  getCarById( @Param('id') id: string ) {
+  getCarById( @Param('id', ParseIntPipe ) id: number ) {
     console.log({id})
 
     // const ID = parseInt(id);
 
-    if (this.cars?.[id] === undefined) {
-      return {'msg': 'Car not exist'}
-    }
-    return this.cars?.[id];
+    // if (this.carsService.findOneById( id ) === undefined) {
+    //   return {'msg': 'Car not exist'}
+    // }
+    return this.carsService.findOneById( id );
     
+  }
+
+  @Post()
+  createCar( @Body() body: any ) {
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar( 
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: any ) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar( @Param('id', ParseIntPipe ) id: number ){
+    return {
+      method: 'delete',
+      id
+    }
   }
 }
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+
+    private cars = [
+        {
+            id: 1,
+            brand: 'Toyota',
+            model: 'Corola'
+        },
+        {
+            id: 2,
+            brand: 'Tesla',
+            model: 'S'
+        },
+        {
+            id: 3,
+            brand: 'Jeep',
+            model: 'Cherokee'
+        },
+    ];
+
+    findAll() {
+        return this.cars;
+    }
+
+    findOneById( id: number ){
+
+        // filter return [] and find return undefined
+
+        const car = this.cars.find( car => car.id === id); 
+
+        if ( !car ) {
+            throw new NotFoundException(`Car with id: ${ id }, not found :(`);
+        }
+
+        return car;
+    }
+
+}
